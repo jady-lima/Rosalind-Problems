3f295c65df77b557337e6f325ec19e0fdf201c1d
Jady Lima <jadylsilva@gmail.com>
2024-11-09 11:26:07 -0300

    Revc

--- src/REVC/revc.py
+++ src/REVC/revc.py
@@ -6,3 +6,24 @@
 
 # Return
 # The reverse complement s^c of s.
+
+def open_file(path):
+    with open(path, "r") as f:
+        return f.read()
+
+def complement(file):
+    s = list(file)
+    for i, l in enumerate(s):
+        if (l == "A"):
+            s[i] = "T"
+        elif (l == "T"):
+            s[i] = "A"
+        elif (l == "C"):
+            s[i] = "G"
+        elif (l == "G"):
+            s[i] = "C"
+    s.reverse()
+    print("".join(s))
+    
+path = "src/REVC/rosalind_revc.txt"
+complement(open_file(path))
